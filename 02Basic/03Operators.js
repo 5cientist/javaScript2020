040ded3dfb843c95f53186533348b3d138e70a9c
Hanshir <hanshirlukku@gmail.com>
2020-07-11 13:54:10 +0530

    added operators and discount soloution

---
+++ 02Basic/03Operators.js
@@ -0,0 +1,25 @@
+// + - / * %
+
+var num1 = 100;
+var num2 = 20;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+
+var answer = num1 > num2;
+console.log(answer);
+
+// d = (L-S)/L * 100
+// a + b * c / d * e
+// a + ((b * c )/ d) * e
+
+var sellingPrice = 799;
+var listingPrice = 199;
+
+var discountPercentage = ((sellingPrice - listingPrice) / sellingPrice) * 100;
+console.log("Discount persentage is : " + discountPercentage);
+
+displayDiscountPersentage =Math.round(discountPercentage);
+console.log(displayDiscountPersentage + "% OFF");
