d0bd5424c283dea34ce7c90da8de8951f6c062f8
Hanshir <hanshirlukku@gmail.com>
2020-07-11 11:52:21 +0530

    added 02Variables-two.js

---
+++ 02Basic/02Variables-two.js
@@ -0,0 +1,18 @@
+const uid = "abc123";
+
+
+var fullName = "Hanshir Lukku";
+var email = "hanshirlukku@gmail.com";
+var password = "123456";
+var confrimPassword = "123456";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+
+// fillNmame = prompt("Enter your name");
+console.log(uid);
+console.log(fullName);
+console.log(email);
+console.log(password);
+console.log(confrimPassword);
+console.log(courseCount);
+console.log(isLoggedInFromGoogle);
