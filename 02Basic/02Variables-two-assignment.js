4f0c50c2b76251a21b049a47b3d60a6358f7a4d6
Hanshir <hanshirlukku@gmail.com>
2020-07-11 12:16:51 +0530

    02Variables-two.js assignment completed

--- 02Basic/02Variables-two-assignment.js
+++ 02Basic/02Variables-two-assignment.js
@@ -1,18 +1,21 @@
-const uid = "abc123";
+//ASSIGNMENT
 
-
-var fullName = "Hanshir Lukku";
+const uid = "abc123456";
+var firstName = "Hanshir";
+var secoundName = "Lukku";
 var email = "hanshirlukku@gmail.com";
 var password = "123456";
 var confrimPassword = "123456";
+var country = "India";
+var state = "kerala";
 var courseCount = 0;
 var isLoggedInFromGoogle = false;
+var isLoggedInFromFacebook = false;
 
 // fillNmame = prompt("Enter your name");
 console.log(uid);
-console.log(fullName);
-console.log("Full Name is :", fullName);
-console.log("Full Name is :" + fullName);
+console.log(firstName);
+console.log(secoundName);
 console.log(email);
 console.log(password);
 console.log(confrimPassword);
@@ -24,8 +27,16 @@ console.log(isLoggedInFromGoogle);
 console.log(`
 
         uniqueID is : ${uid}
-        user ${fullName}
+        user name : ${firstName} ${secoundName}
         and his email is : ${email}
         and uses the password : ${password}
+        country : ${country}
+        lived in : ${state}
+        he logged from google : ${isLoggedInFromGoogle}
+        he logged from facebook: ${isLoggedInFromFacebook}
+
         
+
+
+
 `)
