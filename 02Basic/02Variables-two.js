8d453b002afda5f86acff1c69cbca928072b3ad4
Hanshir <hanshirlukku@gmail.com>
2020-07-11 12:04:18 +0530

    added 02Variables-two.js completed

--- 02Basic/02Variables-two.js
+++ 02Basic/02Variables-two.js
@@ -11,8 +11,24 @@ var isLoggedInFromGoogle = false;
 // fillNmame = prompt("Enter your name");
 console.log(uid);
 console.log(fullName);
+console.log("Full Name is :", fullName);
+console.log("Full Name is :" + fullName);
 console.log(email);
 console.log(password);
 console.log(confrimPassword);
 console.log(courseCount);
 console.log(isLoggedInFromGoogle);
+
+//other way to console.log
+
+console.log(`
+
+        uniqueID is : ${uid}
+        user ${fullName}
+        and his email is : ${email}
+        and uses the password : ${password}
+        
+
+
+
+`)
