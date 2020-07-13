2d38bcc4b09288bd897259e8f583090e5f4de76e
Hanshir <hanshirlukku@gmail.com>
2020-07-13 22:56:49 +0530

    added typeof

--- 02Basic/03Operators.js
+++ 02Basic/03Operators.js
@@ -12,7 +12,7 @@ var answer = num1 > num2;
 console.log(answer);
 
 // d = (L-S)/L * 100
-// a + b * c / d * e
+// a + b * c / d *  e
 // a + ((b * c )/ d) * e
 
 var sellingPrice = 799;
@@ -21,5 +21,9 @@ var listingPrice = 199;
 var discountPercentage = ((sellingPrice - listingPrice) / sellingPrice) * 100;
 console.log("Discount persentage is : " + discountPercentage);
 
-displayDiscountPersentage =Math.round(discountPercentage);
+displayDiscountPersentage = Math.round(discountPercentage);
 console.log(displayDiscountPersentage + "% OFF");
+
+var result = listingPrice > sellingPrice ;
+console.log(result);
+console.log(typeof result);
