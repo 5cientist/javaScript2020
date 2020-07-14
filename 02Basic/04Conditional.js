3cf828b718929c6224f3d30335132d7bc9e4359c
Hanshir <hanshirlukku@gmail.com>
2020-07-14 21:32:28 +0530

    added conditional.js

---
+++ 02Basic/04Conditional.js
@@ -0,0 +1,17 @@
+var temperature;
+
+// todo go to google and get data
+
+temperature = 49;
+
+if (temperature < 20) {
+    console.log("its very cold");
+
+}
+
+if (temperature < 30) {
+    console.log("it moderate");
+
+} else {
+    console.log("its very hot");
+}
