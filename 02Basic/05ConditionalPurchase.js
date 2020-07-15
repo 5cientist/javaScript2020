fc8d1337a0b11852b42f92964c7046596039275a
Hanshir <hanshirlukku@gmail.com>
2020-07-15 20:31:42 +0530

    added conditionalPurchase added

---
+++ 02Basic/05ConditionalPurchase.js
@@ -0,0 +1,20 @@
+// logged in
+//email verified 
+//cardInfo - valid
+// if any one missing stop purchase
+
+
+var isLoggedIn = true;
+var isEmailVerified = true;
+var cardInfo = true;
+
+
+// if (isLoggedIn) {
+//     console.log("Logged in sucess");
+//     if (isEmailVerified) {
+//         console.log("email is verified");
+//         if (cardInfo) {
+//             console.log("you can make purchase");
+//         }
+//     }
+// }
