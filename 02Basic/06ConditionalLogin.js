ab1b56cedd6f27d3143eae4c22fde9f212b6d7ec
Hanshir <hanshirlukku@gmail.com>
2020-07-16 10:56:12 +0530

    added conditionalLogin

---
+++ 02Basic/06ConditionalLogin.js
@@ -0,0 +1,14 @@
+//allow the user to acess the courses
+//login from email
+//login from google
+//login from facebook
+
+var email = true;
+var google = false;
+var facebook = false;
+
+if (email || google || facebook) {
+    console.log("login success");
+
+    
+}
