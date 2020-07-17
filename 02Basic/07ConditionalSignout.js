bdb56f4fac09f8404e1bb7c8ecfdb5183858e926
hanshir <hanshirlukku@gmail.com>
2020-07-17 11:02:05 +0530

    added ConditionalSignout.js

---
+++ 02Basic/07ConditionalSignout.js
@@ -0,0 +1,15 @@
+// show user signout button  if he is authenticated.
+//other wise show login/signup
+
+
+
+// var authenticated = true;
+var authenticated = false;
+
+// if (authenticated) {
+//     console.log("show signout button");
+// }else{
+//     console.log("show login option");
+// }
+
+authenticated ? console.log("signOut button") : console.log("login");
