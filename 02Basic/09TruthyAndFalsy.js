1a1b62731f438654110af3f8ece081e9ee83598a
hanshir <hanshirlukku@gmail.com>
2020-07-20 10:34:07 +0530

    09TruthyAndFalsy.js added

---
+++ 02Basic/09TruthyAndFalsy.js
@@ -0,0 +1,22 @@
+// TODO : falsy 
+//undefined
+// null
+// 0
+// ''
+// NaN
+
+// var user = null;
+
+// if (user) {
+//     console.log("Condition is true");
+// }
+
+// console.log("2"+"2");
+
+
+
+var user = "2";
+
+if (2 === user) {
+    console.log("Condition is true");
+}
