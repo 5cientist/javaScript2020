4270fac6a85597811b218a036c7dd7e01ee6cd59
hanshir <hanshirlukku@gmail.com>
2020-07-22 23:25:46 +0530

    added intermidiate folder

---
+++ 03intermeadiate/01Functions.js
@@ -0,0 +1,16 @@
+function sayHello(name) {
+  console.log("Hello there , hanshir");
+  console.log(`hello there , ${name}, how are you ?`);
+}
+
+// sayHello("hanshir");
+// sayHello("nirmanl");
+
+function namstey() {
+  return "hello in india";
+}
+
+var greetings = namstey();
+
+console.log(greetings);
+console.log(namstey());
