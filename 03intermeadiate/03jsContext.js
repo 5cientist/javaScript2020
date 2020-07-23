ee4534d4810f8a2d81271713a0319cf24fafe827
scientist <hanshirlukku@gmail.com>
2020-07-23 11:11:16 +0530

    added 03jscontext using gui

---
+++ 03intermeadiate/03jsContext.js
@@ -0,0 +1,17 @@
+// sayHello();
+// function sayHello() {
+//     console.log("Hello");
+    
+// }
+
+if (2 === "2") {
+    console.log("This is true");
+    
+}
+var myName = "Hanshir";
+if (myName === window.name) {
+
+    console.log("This is an again a true statement");
+    
+    
+}
