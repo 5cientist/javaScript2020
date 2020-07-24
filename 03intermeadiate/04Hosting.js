9c5a668a0c212906a0ed0b0e64057224970caf24
hanshir <hanshirlukku@gmail.com>
2020-07-24 10:54:33 +0530

    completed hosting

--- 03intermeadiate/04Hosting.js
+++ 03intermeadiate/04Hosting.js
@@ -1,6 +1,28 @@
+tipper("80");
+
 function tipper(a) {
-    var bill = a;
+    var bill = parseInt(a);
     console.log(bill + 5);
     
 }
-tipper(20);
+
+function bigTipper(a) {
+    var bill = parseInt(a);
+    console.log(bill + 15);
+
+    
+};
+
+bigTipper("200");
+
+var name = "Hanshir";
+console.log(name);
+
+function sayName() {
+    var name = "MR.H";
+    console.log(name);
+    
+}
+sayName();
+
+console.log(name);
