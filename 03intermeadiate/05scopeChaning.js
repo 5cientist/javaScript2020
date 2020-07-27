283fa63c1c05865731b4c008a924d53a75284f7b
hanshir <hanshirlukku@gmail.com>
2020-07-27 23:13:50 +0530

    scopeChaning

---
+++ 03intermeadiate/05scopeChaning.js
@@ -0,0 +1,19 @@
+var name = "hanshir";
+
+console.log("Line number 3", name);
+
+function sayName() {
+    var name = "Mr.hii"
+    console.log("Line number 6 ", name);
+
+sayNameTwo();
+
+    function sayNameTwo() {
+        var name = "HL"
+        console.log("Line number 10",name);
+        
+    }
+
+}
+
+sayName();
