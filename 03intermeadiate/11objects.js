a8324e3a4867c45d1cc5c0bf83f4d52dc656a754
hanshir <hanshirlukku@gmail.com>
2020-08-11 21:48:27 +0530

    object file modified

--- 03intermeadiate/11objects.js
+++ 03intermeadiate/11objects.js
@@ -1,9 +1,17 @@
 var user = {
-    firstName : "hanshir";
-    secoundName : "Lukku";
-    userLoginCount : 32;
-    facebookSignedIn : true;
-    role = "admin";
+    firstName : "hanshir",
+    lastName : "Lukku",
+   loginCount : 32,
+    facebookSignedIn : true,
+    role: "Admin",
 };
 
 console.log(user.firstName);
+console.log(user["lastName"]);
+
+console.log(user.loginCount);
+user.loginCount = 55;
+console.log(user.loginCount);
+console.log(user);
+
+console.table(user);
