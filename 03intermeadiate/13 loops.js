bb65bf3d4bff660b82f52f3afc5831b834b03167
hanshir <hanshirlukku@gmail.com>
2020-08-16 18:59:33 +0530

    modified loops

--- 03intermeadiate/13 loops.js
+++ 03intermeadiate/13 loops.js
@@ -1,6 +1,19 @@
 // 13\ loops.js
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+
+// }
+
+// for (let i=0;i<10;i++){
+
+//     console.log(i);
+// }
+
+const myState = ["kerala", "tamilnadu", "delhi", "karnataka"];
+for (let i = 0; i < myState.length; i++) {
+
+    // console.log(i);
+    // console.log([2]);
+    console.log(myState[i]);
 }
