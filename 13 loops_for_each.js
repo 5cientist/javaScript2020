eb773a6fffdd2d389c5bdcc8fcc5e3d3a288c735
hanshir <hanshirlukku@gmail.com>
2020-08-26 13:22:28 +0530

    for each

---
+++ 13 loops_for_each.js
@@ -0,0 +1,10 @@
+const myState = ["kerala","tamilnadu","delhi","karnataka","maharashtra",];
+
+// var i = 0;
+// for (;;) {
+//   if (i > 3) break;
+//   console.log(i);
+//   i++;
+// }
+
+myState.forEach((s) => (console.log(s)));
