42e66ed5af86d5c74fc22c7e7db6aad69668566e
hanshir <hanshirlukku@gmail.com>
2020-08-26 13:09:46 +0530

    While and do while loops

---
+++ 03intermeadiate/13 loops_while.js
@@ -0,0 +1,26 @@
+const myState = ["kerala", "tamilnadu", "delhi", "karnataka"];
+
+/*
+let i = 0;
+ while (i<myState.length){
+    console.log(myState[i]);
+     i++;
+ }
+
+OUTPUT
+kerala
+tamilnadu
+delhi
+karnataka
+*/
+
+let i = 20;
+do {
+  console.log(i);
+  i++;
+} while (i < 10);
+
+/*
+OUTPUT
+20
+*/
