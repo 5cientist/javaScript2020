b45e079e7b8ab71a499f2cbd514b92331b133151
hanshir <hanshirlukku@gmail.com>
2020-09-09 19:57:53 +0530

    event listner project 2 completed

--- project2/script.js
+++ project2/script.js
@@ -5,3 +5,42 @@ const orange = document.querySelector(".orange");
 const pink = document.querySelector(".pink");
 
 const center = document.querySelector(".center");
+
+// console.log(window.getComputedStyle(red).backgroundColor); 
+//to get the background color of red.
+
+
+
+
+// const getBGColor = function (){}
+//            or
+
+
+
+
+const getBGColor = (selectedElement) => {
+    return window.getComputedStyle(selectedElement).backgroundColor;
+};
+
+
+// var color = getBGColor(pink);
+
+// pink.addEventListener("mouseenter", () => {
+//     center.style.background = color;
+     
+// });
+
+const magicColorChanger= (element,color) => {
+    return element.addEventListener("mouseenter", () => {
+        center.style.background = color;
+         
+    });
+
+};
+
+magicColorChanger(red, getBGColor(red));
+magicColorChanger(cyan, getBGColor(cyan));
+magicColorChanger(violet, getBGColor(violet));
+magicColorChanger(orange, getBGColor(orange));
+magicColorChanger(pink, getBGColor(pink));
+
