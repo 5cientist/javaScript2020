cbf8d310c39cd1a78b793f0bc9b6ff6415daaeca
hanshir <hanshirlukku@gmail.com>
2020-09-09 19:20:59 +0530

    queryselector added in js

---
+++ project2/script.js
@@ -0,0 +1,7 @@
+const red = document.querySelector(".red");
+const cyan = document.querySelector(".cyan");
+const violet = document.querySelector(".violet");
+const orange = document.querySelector(".orange");
+const pink = document.querySelector(".pink");
+
+const center = document.querySelector(".center");
