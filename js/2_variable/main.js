a683939f46887f340aba72a789fe636953491502
hanshir <hanshirlukku@gmail.com>
2021-01-17 13:27:47 +0530

    added new variable file

---
+++ js/2_variable/main.js
@@ -0,0 +1,12 @@
+let num1 = 10;
+let num2 = 30;
+
+let result = num1 + num2;
+
+// console.log(num1 + num2);
+// console.log(result);
+// console.log(`the sum of two number is ${result}`);
+
+let happy = document.querySelector(".heading");
+happy.innerHTML = `the sum of two number is ${result}`;
+console.log(happy);
