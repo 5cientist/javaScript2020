772c8da80ceb53c7c93a52092cba52e22beba117
hanshir <hanshirlukku@gmail.com>
2021-01-17 13:53:04 +0530

    javascript array

---
+++ js/03_array/main.js
@@ -0,0 +1,13 @@
+//array
+
+//kitchen items
+
+let saltContainer = "salt";
+let pepperContainer = "pepper";
+let chilliContainer = "chilli";
+let turmericContainer = "turmeric";
+
+//arrray syntax
+let curryPowder = ["salt", "pepper", "chillie", "turmeric"];
+console.log(curryPowder);
+console.log(curryPowder[3]);
