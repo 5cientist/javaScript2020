7a22c5e4bccf98ed6ed69b895d71135bd5c02fea
hanshir <hanshirlukku@gmail.com>
2021-01-20 23:33:56 +0530

    array method 2 added

--- js/03_array_project/main.js
+++ js/03_array_project/main.js
@@ -1,10 +1,26 @@
-let kitchenItemsList = document.getElementById("kitchenItemsList");
+let kitchenItemsList = document.getElementById("kitchen-Items-List");
 let curryPowder = ["salt", "sugar", "chily", "turmeric"];
 // console.log(curryPowder);
 
-for (let powder of curryPowder) {
+// method ONE
+
+// for (let powder of curryPowder) {
+//   let li = document.createElement("li");
+//   li.innerText = powder;
+//   kitchenItemsList.appendChild(li);
+//   console.log(li);
+// }
+
+// Method TWO
+// foreach()
+curryPowder.forEach(function (powder) {
+  // console.log(powder);
   let li = document.createElement("li");
   li.innerText = powder;
   kitchenItemsList.appendChild(li);
-  console.log(li);
-}
+});
+
+//1. push()
+//2. pop()
+//3. shif()
+//4. unshif()
