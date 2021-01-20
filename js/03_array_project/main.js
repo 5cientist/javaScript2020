29e22d36241991368d8d2ec67364bc231e2eb4c7
hanshir <hanshirlukku@gmail.com>
2021-01-20 23:47:35 +0530

    push method in array

--- js/03_array_project/main.js
+++ js/03_array_project/main.js
@@ -13,14 +13,26 @@ let curryPowder = ["salt", "sugar", "chily", "turmeric"];
 
 // Method TWO
 // foreach()
-curryPowder.forEach(function (powder) {
-  // console.log(powder);
-  let li = document.createElement("li");
-  li.innerText = powder;
-  kitchenItemsList.appendChild(li);
-});
+// curryPowder.forEach(function (powder) {
+//   // console.log(powder);
+//   let li = document.createElement("li");
+//   li.innerText = powder;
+//   kitchenItemsList.appendChild(li);
+// });
 
 //1. push()
 //2. pop()
 //3. shif()
 //4. unshif()
+
+//find all the list items here
+let allListItems = document.querySelectorAll("li");
+let powederArray = [];
+
+console.log(allListItems);
+allListItems.forEach(function (listItems) {
+  console.log(listItems);
+  let listItemsText = listItems.innerText;
+  console.log(listItemsText);
+  powederArray.push(listItemsText);
+});
